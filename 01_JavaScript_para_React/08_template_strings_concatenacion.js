4ed53069347b7820cabce6dd2a0e30cf741cffce
jrxjb <177959714+jrxjb@users.noreply.github.com>
2025-02-04 20:37:23 -0400

    add

---
+++ 01_JavaScript_para_React/08_template_strings_concatenacion.js
@@ -0,0 +1,7 @@
+// Template Strings y Concatenacion
+
+const producto = "Tablet de  12 pulgadas";
+const precio = 400;
+const marca = "Orange"
+
+console.log(`El producto es: ${producto} y el precio es: ${precio}`)
---
+++ 01_JavaScript_para_React/09_arrays_o_arreglos.js
@@ -0,0 +1,8 @@
+// Arrays o Arreglos
+// arrys [] y objetos {}
+
+const tecnologias = [20,30,4,true,"hola"] //se recomineda que sean del mismo tipo de dato no mezclar 
+
+console.table(tecnologias)
+
+console.log(tecnologias[1])
